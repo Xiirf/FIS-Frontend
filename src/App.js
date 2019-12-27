5b31472ac1e735bf75fbaa26a3d44745a3bd6c83
Agustín Núñez <seviagus@gmail.com>
2019-12-27 17:34:07 +0100

    Review component implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Review from './components/ReviewComponent/Review.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="container mt-5">
+        <Review id="e4sdfg23fsdf3" username="rick_957" title="Buena peli" rating={4} timestamp="2019-12-27T17:32:36.884+01:00" content="La peli ha estado muy bien. Me ha gustado el actor principal." likes={10} dislikes={0} spams={0} />
+      </div>
+      
     </div>
   );
 }
