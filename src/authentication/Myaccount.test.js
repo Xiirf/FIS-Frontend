c790055696b4b62b0ee40c734528695e149c303d
Quentin Enjalbert <quentin.enjalbert@insa-rouen.fr>
2020-01-05 21:30:18 +0100

    Add tests

--- src/authentication/Myaccount.test.js
+++ src/authentication/Myaccount.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Myaccount from './Myaccount';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { create } from "react-test-renderer";
+
+let container = null;
+beforeEach(() => {
+  // met en place un élément DOM comme cible de rendu
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // nettoie en sortie de test
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Myaccount />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<Myaccount />, container);
+});
+
+it("matches the snapshot", () => {
+    const myaccount = create(<Myaccount />);
+    expect(myaccount.toJSON()).toMatchSnapshot();
 });
--- src/authentication/Signin.test.js
+++ src/authentication/Signin.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Signin from './Signin';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { create } from "react-test-renderer";
+
+let container = null;
+beforeEach(() => {
+  // met en place un élément DOM comme cible de rendu
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // nettoie en sortie de test
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Signin />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<Signin />, container);
+});
+
+it("matches the snapshot", () => {
+    const signin = create(<Signin />);
+    expect(signin.toJSON()).toMatchSnapshot();
 });
